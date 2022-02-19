ff7742f5d32b7065504b3221e9660c4224fb62f4
Nguyen Tung Bach <bachntth2010055@fpt.edu.vn>
2022-02-19 20:01:48 +0700

    bach

---
+++ Thi_UWP/MainPage.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Thi_UWP.Entity;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Thi_UWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.Loaded += MainPage_Loaded;
+        }
+
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<Contact> listContact = Server.DataInitialization.ListContact();
+            ListDataGridCreate.ItemsSource = listContact;
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var contact = new Contact()
+            {
+                Name = name.Text,
+                Phone = phone.Text,
+            };
+            ContentDialog contentDialog = new ContentDialog();
+            if (Server.DataInitialization.IsInsertContact(contact))
+            {
+                contentDialog.Title = "Acction success";
+                contentDialog.Content = "Them thanh cong";
+            }
+            else
+            {
+                contentDialog.Title = "Acction fail";
+                contentDialog.Content = "Them that bai";
+            }
+            contentDialog.CloseButtonText = "OK";
+            await contentDialog.ShowAsync();
+            List<Contact> listContact = Server.DataInitialization.ListContact();
+            ListDataGridCreate.ItemsSource = listContact;
+        }
+
+        private async void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                List<Contact> listContact = Server.DataInitialization.ListTransactionByName(searchName.Text);
+                ListDataGridCreate.ItemsSource = listContact;
+            }
+            catch
+            {
+                ContentDialog contentDialog = new ContentDialog();
+                contentDialog.Title = "Lỗi!";
+                contentDialog.Content = "Contact not found";
+                contentDialog.CloseButtonText = "Oke";
+                await contentDialog.ShowAsync();
+            }
+        }
+    }
+}
---
+++ Thi_UWP/Server/DataInitialization.cs
@@ -0,0 +1,105 @@
+﻿using SQLitePCL;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Thi_UWP.Entity;
+
+namespace Thi_UWP.Server
+{
+    class DataInitialization
+    {
+        public static void CreatedContact()
+        {
+            SQLiteConnection cnn = new SQLiteConnection("contactlite.db");
+            string sqlPersonalContact = @"CREATE TABLE IF NOT EXISTS
+            Contact (Id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+            Name VARCHAR( 140 ),
+            Phone VARCHAR( 140 )
+            );";
+            using (var statement = cnn.Prepare(sqlPersonalContact))
+            {
+                Debug.WriteLine("Tao bang");
+                statement.Step();
+            }
+        }
+
+        public static bool IsInsertContact(Contact contact)
+        {
+            try
+            {
+                SQLiteConnection cnn = new SQLiteConnection("contactlite.db");
+                using (var personalTransaction = cnn.Prepare("INSERT INTO Contact(Name, Phone) VALUES(?, ?)"))
+                {
+                    personalTransaction.Bind(1, contact.Name);
+                    personalTransaction.Bind(2, contact.Phone);
+                    personalTransaction.Step();
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                return false;
+            }
+        }
+        public static List<Contact> ListContact()
+        {
+            var list = new List<Contact>();
+            try
+            {
+                SQLiteConnection cnn = new SQLiteConnection("contactlite.db");
+                using (var stt = cnn.Prepare("select * from Contact"))
+                {
+                    while (stt.Step() == SQLiteResult.ROW)
+                    {
+                        var contactThis = new Contact()
+                        {
+                            Name = (string)stt["Name"],
+                            Phone = (string)stt["Phone"],
+                        };
+                        list.Add(contactThis);
+                    }
+                }
+                //Debug.WriteLine(list[0]);
+                return list;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Co loi list" + ex);
+                return null;
+            }
+        }
+
+        public static List<Contact> ListTransactionByName(string Name)
+        {
+            var list = new List<Contact>();
+            try
+            {
+                Debug.WriteLine("Name la" + Name);
+                SQLiteConnection cnn = new SQLiteConnection("contactlite.db");
+                using (var stt = cnn.Prepare($"select * from Contact where Name = '{Name}'"))
+                {
+                    while (stt.Step() == SQLiteResult.ROW)
+                    {
+                        var contact = new Contact()
+                        {
+                            Name = (string)stt["Name"],
+                            Phone = (string)stt["Phone"],
+                        };
+                        list.Add(contact);
+                    }
+                }
+                Debug.WriteLine(list[0]);
+                return list;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Co loi list" + ex);
+                return null;
+            }
+        }
+    }
+}
